6b9076a09228d916a917543dcfd2837c7bbd2756
Diego Caballero <2123010302@untels.edu.pe>
2024-07-22 15:14:34 -0500

    Caballero Examen Final - Parte 2 - Base de Datos

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Caballero_Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    sexo = models.BooleanField()
+    fecha_de_registro = models.DateTimeField(auto_now_add=True)
